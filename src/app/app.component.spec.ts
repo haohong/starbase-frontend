d2c144add691555c5146b1374d0810e780ce7d5b
Haohong Xu <weidai1122@gmail.com>
2017-10-28 02:46:24 +0800

    Fix HomeComponent spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,43 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
+
+import { BookmarkService } from './shared/services/bookmark.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MaterialModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        BookmarkService
+      ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'stb'`, async(() => {
+
+  it(`should have as title 'Ethereum Transactions Finder'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('stb');
+    expect(app.title).toEqual('Ethereum Transactions Finder');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to stb!');
+    expect(compiled.querySelector('h1').textContent).toContain('Ethereum Transactions Finder');
   }));
 });
